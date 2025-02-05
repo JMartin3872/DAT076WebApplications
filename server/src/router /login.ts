4dd352464f297e04d26acdeec81d0be8293e9b65
narimaun <narimaun@chalmers.se>
2025-02-05 09:45:39 +0100

    FEAT: working on loginRouter

--- server/src/router /login.ts
+++ server/src/router /login.ts
@@ -1,1 +1,10 @@
-//This is the router
+import express, { Router, Request, Response } from "express";
+import {LoginService} from "../service/login";
+
+const loginService : LoginService = new LoginService();
+
+const loginRouter : Router = express.Router();
+
+loginRouter.post("/register", async (
+    req : Request<>, res: Response)
+)
