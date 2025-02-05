7c778dbc0d09b0fdd74cb7fbcd109b9f775f435c
narimaun <narimaun@chalmers.se>
2025-02-05 13:50:05 +0100

    work on routerlogin

--- server/src/router /login.ts
+++ server/src/router /login.ts
@@ -1,10 +1,33 @@
 import express, { Router, Request, Response } from "express";
 import {LoginService} from "../service/login";
+import {Login} from "../model/login";
 
 const loginService : LoginService = new LoginService();
 
 const loginRouter : Router = express.Router();
 
 loginRouter.post("/register", async (
-    req : Request<>, res: Response)
-)
+    req : Request<{},string,{username : string; password : string}>, res: Response<string>
+    ) => {
+    if (typeof (req.body.password) !== "string" || typeof (req.body.username) !== "string"){
+        res.status(400).send(`username and password Need to be strings, password type: ${typeof (req.body.password)} 
+        username type: ${typeof (req.body.username)}`);
+    }
+    else {
+       await loginService.registerUser(req.body.username,req.body.password);
+       res.status(201).send("Created!");
+    }})
+
+loginRouter.post("/login", async (
+    req : Request<{},listOfDiaries,{username : string; password : string}>, res: Response<listOfDiaries>
+    )=> {
+    const loginResult : listOfDiaries | undefined = await loginService.tryLogin(req.body.username,req.body.password)
+    if (loginResult === undefined) {
+        res.status(401).send("wrong credentials");
+    }
+    else {
+        res.status(201).send(loginResult);
+    }})
+
+
+
