0d7ff57bd1724411353fc1ac105901a16cecb2e9
Nomis Novak <narimaun@chalmers.se>
2025-02-07 08:49:40 +0100

    added try catch to Login

--- server/src/router/login.ts
+++ server/src/router/login.ts
@@ -17,39 +17,59 @@ loginRouter.get("/getLogins", async (
 loginRouter.post("/register", async (
     req : Request<{},string,{username : string; password : string}>, res: Response<string>
     ) => {
-    if (typeof (req.body.password) !== "string" || typeof (req.body.username) !== "string"){
-        res.status(400).send(`username and password Need to be strings, password type: ${typeof (req.body.password)} 
+    try {
+        if (typeof (req.body.password) !== "string" || typeof (req.body.username) !== "string") {
+            res.status(400).send(`username and password Need to be strings, password type: ${typeof (req.body.password)} 
         username type: ${typeof (req.body.username)}`);
+        } else {
+            await loginService.registerUser(req.body.username, req.body.password);
+            res.status(201).send("Created!");
+        }
     }
-    else {
-       await loginService.registerUser(req.body.username,req.body.password);
-       res.status(201).send("Created!");
-    }})
+    catch (e : any) {
+        res.status(500).send(e.message);
+    }
+    }
+)
+
 //Router for a user trying to login
-/*
-loginRouter.post("/", async (
+loginRouter.post("/login", async (
     req : Request<{},listOfDiaries,{username : string; password : string}>, res: Response<listOfDiaries>
     )=> {
-    const loginResult : listOfDiaries | undefined = await loginService.tryLogin(req.body.username,req.body.password)
-    if (loginResult === undefined) {
-        res.status(401).send("Wrong credentials");
+    try {
+        const loginResult: listOfDiaries | undefined = await loginService.tryLogin(req.body.username, req.body.password)
+        if (loginResult === undefined) {
+            res.status(401).send("Wrong credentials");
+        } else {
+            res.status(201).send(loginResult);
+        }
     }
-    else {
-        res.status(201).send(loginResult);
-    }})
- */
+    catch(e:any){
+        res.status(500).send(e.message);
+    }
+    }
+    )
+
+
 //To change password of a user
 loginRouter.patch("/changepassword", async (
+
     req : Request<{}, string, {username : string; oldPassword : string; newPassword : string}>,
     res : Response<string>) => {
-    const changePassword : Login | undefined
-        = await loginService.changePassword(req.body.username,req.body.oldPassword,req.body.newPassword);
-    if (changePassword === undefined){
-        res.status(401).send("Wrong credentials");
+    try {
+        const changePassword: Login | undefined
+            = await loginService.changePassword(req.body.username, req.body.oldPassword, req.body.newPassword);
+        if (changePassword === undefined) {
+            res.status(401).send("Wrong credentials");
+        } else {
+            res.status(201).send("success!")
+        }
     }
-    else {
-        res.status(201).send("success!")
-    }})
+    catch (e:any) {
+        res.status(500).send(e.message);
+    }
+    }
+    )
 
 
 
