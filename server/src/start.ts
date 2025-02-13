f3fff597b87944118bb03a63a138aa7bf3249344
NovakN72 <narimaun@chalmers.se>
2025-02-13 08:01:55 +0100

    Added the new client side with all the packages

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import {loginRouter} from "./router/login";
 import { diaryRouter } from "./router/diary";
+import cors from "cors";
+
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/diary", diaryRouter)
 app.use("/login", loginRouter);
