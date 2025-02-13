632498ba7afdcf5803ed905336c918e6375f445a
melissam <melissam@student.chalmers.se>
2025-02-14 00:43:11 +0100

    / Added tests for deleteDiary/
    Co-authored-by: tyra03 <tyra03@users.noreply.github.com>

--- server/src/service/diary.test.ts
+++ server/src/service/diary.test.ts
@@ -6,16 +6,48 @@ let diary : Diary;
 
 beforeEach(() => {
     diaryService = new DiaryService();
-}
 
+});
 
+test('Should delete the diary and return a list of remaining diaries', async () => {
+    await diaryService.createDiary("User1", "Diary1");
+    await diaryService.createDiary("User1", "Diary2");
+
+    const diariesBefore = await diaryService.getListOfDiaries("User1"); 
+    expect(diariesBefore.length).toBe(2);
+
+    const diarydelete  = await diaryService.deleteDiary("User1", 0);
+    const remainingDiaries = await diaryService.getListOfDiaries("User1");
+
+    expect(diarydelete).toEqual(remainingDiaries);
+    expect(remainingDiaries.length).toBe(1);
+    expect(remainingDiaries[0].id).toBe(1);
+    }
 )
+
+
+test('Only owner of a diary should be able to delete it', async () =>{
+   await diaryService.createDiary("User1", "Diary1");
+   await diaryService.createDiary("User2", "Diary1");
+
+   const diaries = await diaryService.deleteDiary("User1", 1);
+   expect(diaries).toBe("Diary not found or unauthorized.");
+
+   const diariesOwner = await diaryService.deleteDiary("User2", 1);
+   const remainingDiaries = await diaryService.getListOfDiaries("User2");
+   expect(remainingDiaries.length).toBe(0);
+}
+);
+
+
+
 
 test('Adding an entry to a diary should return an entry with the correct text', async () => {
     const diary : Diary = {
         id: 0,
         title: "TestDiary",
         owner: "TestUser",
+        nextEntryId: 0,
         entries: []
     };
 
