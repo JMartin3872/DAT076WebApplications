b40f9e7fcdb2de781aea4660d1b27b674163f94c
NovakN72 <narimaun@chalmers.se>
2025-02-25 08:32:07 +0100

    added more tests to backend

--- server/src/service/login.test.ts
+++ server/src/service/login.test.ts
@@ -1,5 +1,6 @@
 import {LoginService} from "./login";
 import {Login} from "../model/login";
+import {log} from "node:util";
 
 let loginService: LoginService;
 
@@ -15,3 +16,26 @@ test('Registering a new login id should return a correct login id!', async () =>
     expect(login.username).toStrictEqual("name1");
     expect(login.password).toStrictEqual("123");
 });
+
+test('Changing a usernames password should return the username with the new password', async () => {
+    await loginService.registerUser("name2","123");
+    const login :Login | undefined = await loginService.changePassword("name2","123","456");
+    if( login) {
+        expect(login.username).toStrictEqual("name2")
+        expect(login.password).toStrictEqual("456");
+    }
+})
+
+test('Changing a usernames password, if the initial password is wrong, it should return undefined', async () => {
+    await loginService.registerUser("name2","123");
+    const login :Login | undefined = await loginService.changePassword("name2","789","456");
+    expect(login).toStrictEqual(undefined)
+})
+
+
+test('Changing a usernames password, if the initial username is wrong, it should return undefined', async () => {
+    await loginService.registerUser("name2","123");
+    const login :Login | undefined = await loginService.changePassword("wrongName","123","456");
+    expect(login).toStrictEqual(undefined)
+})
+
