8d501aa6a3d7f01ed4a7de940a8d23065538e212
tyra03 <tyra.olofsson@hotmail.se>
2025-02-25 11:01:08 +0100

    Backtracked to earlier version of router/login

--- server/src/router/login.ts
+++ server/src/router/login.ts
@@ -7,84 +7,72 @@ const loginService : LoginService = new LoginService();
 
 export const loginRouter : Router = express.Router();
 
-interface UserRequest extends Request {
-    body: { username: string, password: string },
-    session: any
-}
-
 loginRouter.get("/", async (
-    req : Request, res : Response) => {
-    const list = await loginService.getLogin();
-    res.status(201).send(list);
+        req : Request, res : Response) => {
+        const list = await loginService.getLogin();
+        res.status(201).send(list);
     }
 )
 
 //Router for registering a new user
 loginRouter.post("/register", async (
-    req : Request<{},string,{username : string; password : string}>, res: Response<string>
+        req : Request<{},string,{username : string; password : string}>, res: Response<string>
     ) => {
-    try {
-        if (typeof (req.body.password) !== "string" || typeof (req.body.username) !== "string") {
-            res.status(400).send(`username and password Need to be strings, password type: ${typeof (req.body.password)} 
+        try {
+            if (typeof (req.body.password) !== "string" || typeof (req.body.username) !== "string") {
+                res.status(400).send(`username and password Need to be strings, password type: ${typeof (req.body.password)} 
         username type: ${typeof (req.body.username)}`);
-        } else {
-            await loginService.registerUser(req.body.username, req.body.password);
+            } else {
+                await loginService.registerUser(req.body.username, req.body.password);
 
-            res.status(201).send("Created!");
+                res.status(201).send("Created!");
+            }
         }
-    }
-    catch (e : any) {
-        res.status(500).send(e.message);
-    }
+        catch (e : any) {
+            res.status(500).send(e.message);
+        }
     }
 )
 
-
+//TODO: Fix request session as well!
 //Router for a user trying to login
 loginRouter.post("/", async (
-    req : UserRequest,
-    res: Response<Diary[] | string>
+        req : Request<{},Diary[] | string,{username : string; password : string}>,
+        res: Response<Diary[] | string>
     )=> {
-    try {
-        if (req.session.username === req.body.username){
-            delete req.session.username   //If the user is already logged in, log out and log in as this user
-        }
-        const loginResult: Diary[] | undefined = await loginService.tryLogin(req.body.username, req.body.password)
-        if (loginResult === undefined) {
-            res.status(401).send("Wrong credentials");
-        } else {
-            req.session.username = req.body.username //Giving a token to the user just logged in
-            res.status(200).send(loginResult);
-        }
-    }
-    catch(e:any){
-        res.status(500).send(e.message);
-    }
-    }
-    )
+        try {
+            const loginResult: Diary[] | undefined = await loginService.tryLogin(req.body.username, req.body.password)
+            if (loginResult === undefined) {
+                res.status(401).send("Wrong credentials");
+            } else {
+                //  (req as any).session.username = req.body.username;
+                res.status(200).send(loginResult);
 
-
+            }
+        }
+        catch(e:any){
+            res.status(500).send(e.message);
+        }
+    }
+)
 
 
 //To change password of a user
 loginRouter.patch("/changepassword", async (
 
-    req : Request<{}, string, {username : string; oldPassword : string; newPassword : string}>,
-    res : Response<string>) => {
-    try {
-        const changePassword: Login | undefined
-            = await loginService.changePassword(req.body.username, req.body.oldPassword, req.body.newPassword);
-        if (changePassword === undefined) {
-            res.status(401).send("Wrong credentials");
-        } else {
-            res.status(201).send("success!")
+        req : Request<{}, string, {username : string; oldPassword : string; newPassword : string}>,
+        res : Response<string>) => {
+        try {
+            const changePassword: Login | undefined
+                = await loginService.changePassword(req.body.username, req.body.oldPassword, req.body.newPassword);
+            if (changePassword === undefined) {
+                res.status(401).send("Wrong credentials");
+            } else {
+                res.status(201).send("success!")
+            }
+        }
+        catch (e:any) {
+            res.status(500).send(e.message);
         }
     }
-    catch (e:any) {
-        res.status(500).send(e.message);
-    }
-    }
-    )
-
-
-
+)
