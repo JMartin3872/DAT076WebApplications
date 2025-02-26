3c39e99a91782ab4a4eaf471a315d127f7cf7e54
Martin Berntsson <j.martin.berntsson@gmail.com>
2025-02-26 10:44:45 +0100

    Entry dates are now displayed correctly and removed unused template files from react.

--- client/src/diary_components/entryComponent.tsx
+++ client/src/diary_components/entryComponent.tsx
@@ -1,7 +1,7 @@
-import {useState} from "react";
-import {Card, Row, Button, Col} from "react-bootstrap";
+import { useState } from "react";
+import { Card, Row, Button, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Entry} from "../api.ts";
+import { Entry } from "../api.ts";
 
 type EntryComponentProps = {
     myEntry: Entry;
@@ -14,36 +14,37 @@ export function EntryComponent({ myEntry, onDelete }: EntryComponentProps) {
     const [entry] = useState<Entry>(myEntry)
 
     if (!myEntry) return null;
-    
-    return(
+
+    return (
         <>
             <div>
-                
-                    <Card border="dark" style={{minWidth: '45rem'}} bg='light'>
-                            <Card.Header>
-                                <Row>
-                                    <Col className="text-start">
-                                        
-                                        {entry.date}
-                                        
-                                    </Col>
+                <p>
+                    <Card border="dark" style={{ minWidth: '45rem' }} bg='light'>
+                        <Card.Header>
+                            <Row>
+                                <Col className="text-start datetext">
 
-                                    <Col className="text-end">
-                                        <Button className="diarybutton" 
-                                            variant="outline-danger"
-                                            onClick={() => onDelete(entry.id)}>
-                                            Delete
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </Card.Header>
+                                    {new Date(entry.date).toLocaleString()}
 
-                            
-                            <Card.Body className="text-start">
-                                {entry.text}
-                            </Card.Body>
+                                </Col>
+
+                                <Col className="text-end">
+                                    <Button className="diarybutton"
+                                        variant="outline-danger"
+                                        onClick={() => onDelete(entry.id)}>
+                                        Delete
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Card.Header>
+
+
+                        <Card.Body className="text-start">
+                            {entry.text}
+                        </Card.Body>
                     </Card>
-           
+                </p>
+
             </div>
         </>
     );
