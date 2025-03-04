86b5d3a94904bfa5cf1d791d0d077df98f1fbc99
Martin <martbern@chalmers.se>
2025-03-04 18:04:27 +0100

    Changed to using authentication for db

--- server/db/conn.ts
+++ server/db/conn.ts
@@ -1,4 +1,8 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize} from 'sequelize';
+import dotenv from "dotenv";
+import { DiaryModel } from './diary.db';
+
+dotenv.config();
 
 export let conn: Sequelize;
 
@@ -11,9 +15,36 @@ if (process.env.NODE_ENV === "test") {
 }
 // Else setup connection for the real db
 else {
-    conn = new Sequelize('postgres://postgres:Commando450@localhost:5432/');
+    const db_password = process.env.DB_PASSWORD;
+
+    if(!db_password){
+        console.log("Could not find DB_PASSWORD in .env file")
+    }
+
+    else{
+        
+        conn = new Sequelize({
+            dialect : 'postgres',
+            username: 'app_db_user',
+            password: db_password,
+            host: 'localhost',
+            port: 5432,
+            database: 'postgres'
+        })
+    }
 }
 
 export async function initDB() {
-    await conn.sync({ alter: true, force: false });
+    try {
+        await conn.authenticate();
+        console.log('Connection has been established successfully.');
+        
+        // Try syncing and creating the table if it doesn't exist
+        await conn.sync({ alter: true, force: false });
+        console.log('Database sync complete.');
+
+       
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
 }
