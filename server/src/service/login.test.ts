7a0deb2aa9a63c7a989bc3f6dcb73945b7443b67
NovakN72 <narimaun@chalmers.se>
2025-03-04 09:30:40 +0100

    Fixed backend login tests

--- server/src/service/login.test.ts
+++ server/src/service/login.test.ts
@@ -1,6 +1,6 @@
 import {LoginService} from "./login";
 import {Login} from "../model/login";
-import {log} from "node:util";
+import bcrypt from "bcrypt";
 
 let loginService: LoginService;
 
@@ -13,16 +13,18 @@ test('Getting the initial list of login credentials mus be a empty list!', async
 
 test('Registering a new login id should return a correct login id!', async () => {
     const login: Login = await loginService.registerUser("name1", "123");
+    const result = await bcrypt.compare("123",login.password)
     expect(login.username).toStrictEqual("name1");
-    expect(login.password).toStrictEqual("123");
+    expect(result).toStrictEqual(true)
 });
 
 test('Changing a usernames password should return the username with the new password', async () => {
     await loginService.registerUser("name2","123");
     const login :Login | undefined = await loginService.changePassword("name2","123","456");
     if( login) {
+        const result = await bcrypt.compare("456",login.password)
         expect(login.username).toStrictEqual("name2")
-        expect(login.password).toStrictEqual("456");
+        expect(result).toStrictEqual(true)
     }
 })
 
