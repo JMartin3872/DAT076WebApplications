f25ecc8133b2d71dcfb19bf8059042a337ca28a7
NovakN72 <narimaun@chalmers.se>
2025-03-05 13:21:03 +0100

    working on login database

---
+++ server/db/login.db.ts
@@ -0,0 +1,26 @@
+import { Model, InferAttributes, InferCreationAttributes, DataTypes } from 'sequelize';
+import { conn } from './conn';
+
+export class LoginModel extends Model<InferAttributes<LoginModel>, InferCreationAttributes<LoginModel>> {
+    declare username: string;
+    declare password: string;
+}
+
+
+LoginModel.init(
+    {
+        username: {
+            type: DataTypes.STRING,
+            primaryKey:true
+        },
+        password: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize: conn,
+        tableName: "loginTable",
+        timestamps: false,
+
+    }
+)
